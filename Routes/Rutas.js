edd73e73af35640568fe40b5d464d2fd1b9a7638
Andrés Prieto <pipeprieto0902@gmail.com>
2023-04-14 12:00:24 -0500

    Update:Documentacion

--- Routes/Rutas.js
+++ Routes/Rutas.js
@@ -15,9 +15,9 @@ const controlador = require("../conroller/voteController");
  *             schema:
  *               type: string
  *               example: Servidor Iniciado correctamente
- *               
+ *
  */
-rutas.get('/',controlador.init)
+rutas.get("/", controlador.init);
 
 /**
  * @openapi
@@ -40,21 +40,78 @@ rutas.get('/',controlador.init)
  *                   description: dirección hash de la cartera
  *               example:
  *                 _id:'6356aa975aa36a6006f1ad15'
- *                 hash:'sdjansidqwhiasdp20489'               
+ *                 hash:'sdjansidqwhiasdp20489'
  */
-rutas.get("/cartera", controlador.getCarteras)
+rutas.get("/cartera", controlador.getCarteras);
 
+/**
+ * @openapi
+ * /candidatos:
+ *   get:
+ *     description: Obtener listado de candidatos
+ *     responses:
+ *       200:
+ *         description: OK.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                  type: string
+ *                  description: Id del candidato
+ *                 nombre:
+ *                  type: string
+ *                  description: nombre del candidato
+ *                 partido:
+ *                  type: string
+ *                  description: nombre del partido del candidato
+ *                 foto:
+ *                  type: string
+ *                  description: url de la foto del candidato
+ *               example:
+ *                 _id:'634a0cc1e4ad3231d85d2682'
+ *                 nombre:'Gustavo Petro'
+ *                 partido:'Colombia humana'
+ *                 foto:'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Presidente_Gustavo_Petro_Urrego.jpg/640px-Presidente_Gustavo_Petro_Urrego.jpg'
+ */
+rutas.get("/candidatos", controlador.getCandidatos);
 
-rutas.get("/candidatos", controlador.getCandidatos)
+rutas.post("/login", controlador.login);
 
+/**
+ * @openapi
+ * /crearcartera:
+ *   post:
+ *     description: registrar cartera en base de datos
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             hash:
+ *               type: string
+ *               description: direccion hash de la cartera
+ *     responses:
+ *       200:
+ *         description: OK.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                  type: string
+ *                  description: Id de la cartera
+ *                 hash:
+ *                   type: string
+ *                   description: dirección hash de la cartera
+ *               example:
+ *                 _id:'6356aa975aa36a6006f1ad15'
+ *                 hash:'sdjansidqwhiasdp20489'
+ */
+rutas.post("/crearcartera", controlador.createCartera);
 
-rutas.post("/login",controlador.login)
-
-
-rutas.post("/crearcartera",controlador.createCartera)
-
-
-rutas.patch("/updateUser", controlador.updateUser)
-
+rutas.patch("/updateUser", controlador.updateUser);
 
 module.exports = rutas;
